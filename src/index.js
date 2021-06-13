68c392b55d4b036659f177d5c8094fc1abce9af6
Coding Daria <dasha.fbi@gmail.com>
2021-06-13 18:07:25 +0300

    add redux persistor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
-import store, { history } from './redux';
+import { history, getStore } from './redux';
 
 import Root from './root';
 import './assets/css/style.scss';
 
+const { store, persistor } = getStore();
+
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Root />
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Root />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
